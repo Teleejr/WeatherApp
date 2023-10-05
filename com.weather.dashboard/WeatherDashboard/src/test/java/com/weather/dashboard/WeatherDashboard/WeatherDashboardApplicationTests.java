998b8994906535c2397cd5899d2571b2b4f698d2
Teleejr <81259920+Teleejr@users.noreply.github.com>
2023-10-05 16:09:03 -0400

    Initial commit

---
+++ com.weather.dashboard/WeatherDashboard/src/test/java/com/weather/dashboard/WeatherDashboard/WeatherDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.dashboard.WeatherDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
